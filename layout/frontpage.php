495c22026d7809cff1e60d0e27bd84a1d58ba55d
Ronnald Rezende Machado <rrmachado@id.uff.br>
2016-09-23 16:43:25 -0300

    change Layout

--- layout/frontpage.php
+++ layout/frontpage.php
@@ -18,12 +18,12 @@ echo $OUTPUT->doctype() ?>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-    
+
     <?php if ($hasanalytics) { ?>
     <!-- Start Google Analytics -->
     <?php require_once(dirname(__FILE__).'/includes/analytics.php'); ?>
     <!-- End Google Analytics -->
-    <?php } ?>    
+    <?php } ?>
 </head>
 
 <body <?php echo $OUTPUT->body_attributes(); ?>>
@@ -32,48 +32,64 @@ echo $OUTPUT->doctype() ?>
 
     <?php require_once(dirname(__FILE__).'/includes/header.php'); ?>
 
-    <div id="page"> 
-        
-        
+    <div id="page">
+
+        <p class="dashed_line"></p>
         <div class="marketing-wrapper">
-            <?php require_once(dirname(__FILE__).'/includes/home-hero-slideshow.php');?> 
-        </div><!--//marketing-wrapper-->             
-            
+            <?php require_once(dirname(__FILE__).'/includes/home-hero-slideshow.php');?>
+        </div><!--//marketing-wrapper-->
+
         <div class="page-wrapper page-wrapper-loggedin">
-            <div class="container">    
+            <div class="container">
                 <div id="page-content" class="row">
-     
                     <?php /*
                     <div id="page-navbar" class=" col-md-12 clearfix">
                     	<div class="breadcrumb-nav"><?php echo $OUTPUT->navbar(); ?></div>
                     	<nav class="breadcrumb-button"><?php echo $OUTPUT->page_heading_button(); ?></nav>
                 	</div>
                 	*/ ?>
-                    
                    <?php if ($hasleftsidebar) { ?>
                     <div id="region-main" class="col-md-9 col-sm-12 col-xs-12 pull-right">
                     <?php } else { ?>
-                    <div id="region-main" class="col-md-9 col-sm-12 col-xs-12">
+                    <div id="region-main" class="hidden col-md-9 col-sm-12 col-xs-12">
                     <?php } ?>
-        
                         <?php
                         echo $OUTPUT->course_content_header();
                         echo $OUTPUT->main_content();
                         echo $OUTPUT->course_content_footer();
                         ?>
-                        
                     </div>
-                    <?php if ($hasleftsidebar) { ?>
-                    <?php echo $OUTPUT->blocks('side-pre', 'col-md-3 col-sm-12 col-xs-12 pull-left'); ?>
-                    <?php } else { ?>
-                    <?php echo $OUTPUT->blocks('side-pre', 'col-md-3 col-sm-12 col-xs-12 pull-right'); ?>
-                    <?php } ?>
-                </div><!--//page-content-->            
-            </div><!--//container-->        
+                </div><!--//page-content-->
+            </div><!--//container-->
         </div><!--page-wrapper-->
-    	
+        <p class="dashed_line"></p>
     </div><!--//#page-->
-    
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 col-sm-12 col-xs-12">
+                <h4>Acesso ao ambiente Virtual</h4>
+                <div class=""></div>
+                <div class="well">
+                    <form class="form-inline" action="<?php echo $CFG->httpswwwroot; ?>/login/index.php" method="post" id="login"  <?php echo $autocomplete; ?>>
+                      <div class="form-group">
+                        <label for="email">Email address:</label>
+                        <input type="text" name="username" id="username" size="15" placeholder="Matrícula" value="<?php p($frm->username) ?>">
+                      </div>
+                      <div class="form-group">
+                        <label for="pwd">Password:</label>
+                        <input type="password" name="password" id="password" size="15" placeholder="**********" value="" <?php echo $autocomplete; ?>"">
+                      </div>
+                      <div class="checkbox">
+                        <label><input input type="submit" id="loginbtn" value="<?php print_string("login") ?>"> Remember me</label>
+                        <input type="checkbox"></input>
+                      </div>
+                    </form>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
     <?php require_once(dirname(__FILE__).'/includes/footer.php'); ?>
 
 
@@ -82,10 +98,10 @@ echo $OUTPUT->doctype() ?>
 <!-- Frontpage Javascript -->
 <script>
     jQuery(document).ready(function($) {
-        
-        /* ======= Header Background Slideshow - Flexslider ======= */    
+
+        /* ======= Header Background Slideshow - Flexslider ======= */
         /* Ref: https://github.com/woothemes/FlexSlider/wiki/FlexSlider-Properties */
-    
+
         $('#bg-slider').flexslider({
             animation: "fade",
             directionNav: false, //remove the default direction-nav - https://github.com/woothemes/FlexSlider/wiki/FlexSlider-Properties
@@ -95,7 +111,7 @@ echo $OUTPUT->doctype() ?>
                  $(this).find('.slide').css("display", "block"); //prevent flash of the images
             },
         });
-    
+
     });
 </script>
 
--- layout/general.php
+++ layout/general.php
@@ -1,7 +1,7 @@
 <?php
 $hasleftsidebar = (empty($PAGE->theme->settings->layout)) ? false : $PAGE->theme->settings->layout;
 $hasanalytics = (empty($PAGE->theme->settings->useanalytics)) ? false : $PAGE->theme->settings->useanalytics;
-echo $OUTPUT->doctype() 
+echo $OUTPUT->doctype()
 ?>
 <html <?php echo $OUTPUT->htmlattributes(); ?>>
 <head>
@@ -18,12 +18,12 @@ echo $OUTPUT->doctype()
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-    
+
     <?php if ($hasanalytics) { ?>
     <!-- Start Google Analytics -->
     <?php require_once(dirname(__FILE__).'/includes/analytics.php'); ?>
     <!-- End Google Analytics -->
-    <?php } ?>    
+    <?php } ?>
 </head>
 
 <body <?php echo $OUTPUT->body_attributes(); ?>>
@@ -31,7 +31,7 @@ echo $OUTPUT->doctype()
 
     <?php require_once(dirname(__FILE__).'/includes/header.php'); ?>
 
-    <div id="page" class="container">    
+    <div id="page" class="container">
         <div id="page-content" class="row">
             <div id="page-navbar" class=" col-md-12 clearfix">
             	<div class="breadcrumb-nav"><?php echo $OUTPUT->navbar(); ?></div>
@@ -55,8 +55,8 @@ echo $OUTPUT->doctype()
             <?php } else { ?>
             <?php echo $OUTPUT->blocks('side-pre', 'col-md-3 col-sm-12 col-xs-12 pull-right'); ?>
             <?php } ?>
-        </div><!--//page-content-->            
-    </div><!--//#page-->        
+        </div><!--//page-content-->
+    </div><!--//#page-->
 
 
 
